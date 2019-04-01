fd3501f4e821c2b66bbb95fd17e6eee388a8e2dc
Deanna Green <deanna@16vmini.co.uk>
2019-04-01 15:15:14 +0100

    can now go to a url ending with cat and see an image of a cat

--- app.rb
+++ app.rb
@@ -8,6 +8,8 @@ get '/secret' do
   "Secret Page"
 end
 
-get '/hello' do
-  "Hello"
+get '/cat' do
+  "<div>
+    <img src='http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png'
+    </div>"
 end
